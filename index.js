17e57d17b85c7e352a2ace5746ea96658c8a6e09
Jose Mendonca <kitnos@gmail.com>
2016-09-07 01:17:14 +0100

    Bot 0

--- index.js
+++ index.js
@@ -19,7 +19,31 @@ app.get('/webhook', function(req, res) {
         console.log("Validating webhook");
         res.status(200).send(req.query['hub.challenge']);
     } else {
-        console.error("Failed validation. Make sure the validation tokens match.");
-        res.sendStatus(403);
+        var events = req.body.entry[0].messaging;
+        for (i = 0; i < events.length; i++) {
+            var event = events[i];
+            if (event.message && event.message.text) {
+                sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+            }
+        }
+        res.sendStatus(200);
     }
 });
+
+function sendMessage(recipientId, message) {
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+        method: 'POST',
+        json: {
+            recipient: {id: recipientId},
+            message: message,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
+};
